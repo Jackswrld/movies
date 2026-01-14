d9f95e93f45253167acf0be910e1e956e7fcb1bb
jacks wrld <153364853+Jackswrld@users.noreply.github.com>
2026-01-14 23:09:13 +0100

    removed Unnecessary files and added a 404 page to the app.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { Settings } from './settings/settings';
 import { Home } from './home/home';
 import { Categories } from './categories/categories';
 import { Library } from './library/library';
+import { Redirect } from './redirect/redirect';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: Home},
     {path: 'categories', component: Categories},
     {path: 'settings', component: Settings},
-    {path: 'library', component: Library}
+    {path: 'library', component: Library},
+
+
+    {path: '**', component: Redirect}
 ];
---
+++ src/app/redirect/redirect.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-redirect',
+  imports: [RouterModule],
+  templateUrl: './redirect.html',
+  styleUrl: './redirect.css',
+})
+export class Redirect {
+
+}
